718015f0e5ca3b62500d14cc746cedbd9f91782a
Дамјан Георгиевски <gdamjan@gmail.com>
2022-11-26 12:51:32 +0100

    mqtt client from lunatic
    * connect to local mqtt server, works ✓
    * periodically send pings, works ✓
    * receive packets from mqtt, works ✓
    * respond to pings, works ✓

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Name
+
+Marketing blurb
+
+## The goal
 1. read all .wasm modules from a given directory
    check interface, load them, start as actors
 2. connect to mqtt server
@@ -5,8 +10,20 @@
 4. broadcast all received mqtt messages to wasm actors/modules
 5. if actors respond, send to mqtt
 
+## Quick start
 
-ref:
+```
+sudo systemctl start mosquitto.service
+rustup target add wasm32-wasi
+cargo run
+```
+
+## References
 * https://github.com/lunatic-solutions/lunatic-rs
 * https://github.com/SquattingSocrates/mqtt_packet
 * https://github.com/SquattingSocrates/m-cutie-tea
+
+## Maybe
+* reload
+* web dashboard
+* filtering/routing (instead of full broadcast)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,60 @@
-use lunatic::{net::{TcpStream, TlsStream, ToSocketAddrs}, Mailbox};
+use std::time::Duration;
+
+use lunatic::Mailbox;
+use mqtt::control::ConnectReturnCode;
+use mqtt::packet::{
+    ConnackPacket, ConnectPacket, DisconnectPacket, PingrespPacket, VariablePacket, PingreqPacket,
+};
+use mqtt::{Decodable, Encodable};
 
 #[lunatic::main]
 fn main(_: Mailbox<()>) {
-    println!("Hello world");
-    let c = TcpStream::connect("127.0.0.1:1883").unwrap();
+    println!("Lunatic mqtt wasm: starting…");
+    const HEARTBEAT: u16 = 60;
+
+    let mut stream = lunatic::net::TcpStream::connect("127.0.0.1:1883").unwrap();
+    // let stream = net::TlsStream::connect("127.0.0.1:8883").unwrap();
+
+    let mut connect = ConnectPacket::new("37365bd3-3a50-465d-acdc-8ba432c4ecfb");
+    connect.set_clean_session(true);
+    connect.set_keep_alive(HEARTBEAT*2);
+
+    connect
+        .encode(&mut stream)
+        .expect("Failed to send ConnectPacket");
+    let connack = ConnackPacket::decode(&mut stream).expect("Expected ConnackPacket");
+    if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
+        panic!(
+            "Failed to connect to server, return code {:?}",
+            connack.connect_return_code()
+        );
+    }
+
+    let pinger = lunatic::spawn_link!(|stream = {stream.clone()}| {
+        loop {
+            lunatic::sleep(Duration::from_secs((HEARTBEAT).into()));
+            PingreqPacket::new().encode(&mut stream).expect("Ping request sending failed");
+        }
+    });
+
+    println!("Loop started");
+    loop {
+        let packet = VariablePacket::decode(&mut stream)
+            .expect("Decoding a mqtt packet failed");
+        match packet {
+            VariablePacket::PingreqPacket(..) => {
+                let resp = PingrespPacket::new();
+                resp.encode(&mut stream).expect("Ping response sending failed");
+            }
+            VariablePacket::DisconnectPacket(..) => {
+                break;
+            }
+            _ => {},
+        };
+    }
+    pinger.kill();
+    DisconnectPacket::default()
+        .encode(&mut stream)
+        .expect("Failed to send DisconnectPacket");
 
 }
